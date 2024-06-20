7c3b3fbdba8fc1831ff35fa555035afb620c2e20
Piterxus <piterxus@gmail.com>
2024-06-20 11:33:14 +0200

    fix: Error import dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), astroImageTools()]
+  integrations: [preact(), astroImageTools]
 });

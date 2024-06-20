4eb2db950bab3c1a47c2c5993138b8394e577e0d
Piterxus <piterxus@gmail.com>
2024-06-20 09:37:10 +0200

    chore: leave background images pending

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
+import { astroImageTools } from "astro-imagetools";
 
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact(), astroImageTools()]
 });

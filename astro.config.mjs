a96ef363ff1b2b08308d5aee03c1203b7c377a1d
Piterxus <piterxus@gmail.com>
2024-06-21 09:50:42 +0200

    chore: Change image path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { astroImageTools } from "astro-imagetools";
 
 import preact from "@astrojs/preact";
 
+const BASE_URL = process.env.BASE_URL || "/";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), astroImageTools]
+  integrations: [preact(), astroImageTools],
+
 });

9113d953745f897e327755f43f3d0cdf93db2aff
Piterxus <piterxus@gmail.com>
2024-07-31 13:27:47 +0200

    style: Add animation to comments

--- src/components/comments.tsx
+++ src/components/comments.tsx
@@ -1,11 +1,13 @@
 import styles from '../styles/Comments.module.css';
 const commentsIcon = "/imgs/vampire-teeth.svg";
+const comic = "/imgs/comic.png";
 
 export default function Comments() {
 
     return (
-        <div >
-           <img className={styles.icon} src={commentsIcon} alt="" />
+        <div className={styles.commentsContainer}>
+            <img className={styles.icon} src={commentsIcon} alt="Comments icon" />
+            <img className={styles.comic} src={comic} alt="Image with message to encourage commenting" />
         </div>
-    );  
+    );
 }

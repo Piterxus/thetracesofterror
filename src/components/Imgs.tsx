cd9bde10f98582fe1bb4580ae6e920786354cdfc
Piterxus <piterxus@gmail.com>
2024-08-01 15:49:20 +0200

    style: Fixed header

--- src/components/Imgs.tsx
+++ src/components/Imgs.tsx
@@ -189,7 +189,7 @@ export default function Imgs() {
                             )}
 
                         </div>
-                       
+                        <Comments />
                     </div>
                 </div>
             ))}
